b1d8c98113403a9072b36d84333a6ee0b6d199ca
jmccallum-rbi <jamiemccallum0@gmail.com>
2023-09-16 20:18:40 +0100

    finished CRUD app

--- crud/pkg/handlers/UpdateBook.go
+++ crud/pkg/handlers/UpdateBook.go
@@ -35,7 +35,7 @@ func UpdateBook(w http.ResponseWriter, r *http.Request) {
 			w.Header().Add("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
 
-			json.NewEncoder(w).Encode("Updated")
+			json.NewEncoder(w).Encode("Updated Book")
 			break
 		}
 	}
